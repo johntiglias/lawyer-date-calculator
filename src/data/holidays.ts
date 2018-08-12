8ec8df54a5b4b323ff217eda0c9817cc6329e2ff
John Tiglias <john.tiglias@ibm.com>
2018-08-12 00:51:33 -0400

    updating date object constructor again and updating verbage

--- src/data/holidays.ts
+++ src/data/holidays.ts
@@ -1,111 +1,111 @@
 const holidays = [
   {
     name: 'MLK Day 2019',
-    date: new Date('2019-01-21'),
+    date: new Date('2019/01/21'),
   },
   {
     name: 'MLK Day 2020',
-    date: new Date('2020-01-20'),
+    date: new Date('2020/01/20'),
   },
   {
     name: 'MLK Day 2021',
-    date: new Date('2021-01-18'),
+    date: new Date('2021/01/18'),
   },
   {
     name: 'MLK Day 2022',
-    date: new Date('2022-01-17'),
+    date: new Date('2022/01/17'),
   },
   {
     name: 'Presidents Day 2019',
-    date: new Date('2019-02-18'),
+    date: new Date('2019/02/18'),
   },
   {
     name: 'Presidents Day 2020',
-    date: new Date('2020-02-17'),
+    date: new Date('2020/02/17'),
   },
   {
     name: 'Presidents Day 2021',
-    date: new Date('2021-02-15'),
+    date: new Date('2021/02/15'),
   },
   {
     name: 'Presidents Day 2022',
-    date: new Date('2022-02-21'),
+    date: new Date('2022/02/21'),
   },
   {
     name: 'Memorial Day 2019',
-    date: new Date('2019-05-27'),
+    date: new Date('2019/05/27'),
   },
   {
     name: 'Memorial Day 2020',
-    date: new Date('2020-05-25'),
+    date: new Date('2020/05/25'),
   },
   {
     name: 'Memorial Day 2021',
-    date: new Date('2021-05-31'),
+    date: new Date('2021/05/31'),
   },
   {
     name: 'Memorial Day 2022',
-    date: new Date('2022-05-30'),
+    date: new Date('2022/05/30'),
   },
   {
     name: 'Labor Day 2018',
-    date: new Date('2018-09-03'),
+    date: new Date('2018/09/03'),
   },
   {
     name: 'Labor Day 2019',
-    date: new Date('2019-09-02'),
+    date: new Date('2019/09/02'),
   },
   {
     name: 'Labor Day 2020',
-    date: new Date('2020-09-07'),
+    date: new Date('2020/09/07'),
   },
   {
     name: 'Labor Day 2021',
-    date: new Date('2021-09-06'),
+    date: new Date('2021/09/06'),
   },
   {
     name: 'Labor Day 2022',
-    date: new Date('2022-09-05'),
+    date: new Date('2022/09/05'),
   },
   {
     name: 'Colubmus Day 2018',
-    date: new Date('2018-10-08'),
+    date: new Date('2018/10/08'),
   },
   {
     name: 'Colubmus Day 2019',
-    date: new Date('2019-10-14'),
+    date: new Date('2019/10/14'),
   },
   {
     name: 'Colubmus Day 2020',
-    date: new Date('2020-10-12'),
+    date: new Date('2020/10/12'),
   },
   {
     name: 'Colubmus Day 2021',
-    date: new Date('2021-10-11'),
+    date: new Date('2021/10/11'),
   },
   {
     name: 'Colubmus Day 2022',
-    date: new Date('2022-10-10'),
+    date: new Date('2022/10/10'),
   },
   {
     name: 'Thanksgiving Day 2018',
-    date: new Date('2018-11-22'),
+    date: new Date('2018/11/22'),
   },
   {
     name: 'Thanksgiving Day 2019',
-    date: new Date('2019-11-28'),
+    date: new Date('2019/11/28'),
   },
   {
     name: 'Thanksgiving Day 2020',
-    date: new Date('2020-11-26'),
+    date: new Date('2020/11/26'),
   },
   {
     name: 'Thanksgiving Day 2021',
-    date: new Date('2021-11-25'),
+    date: new Date('2021/11/25'),
   },
   {
     name: 'Thanksgiving Day 2022',
-    date: new Date('2022-11-24'),
+    date: new Date('2022/11/24'),
   },
 ];
 
